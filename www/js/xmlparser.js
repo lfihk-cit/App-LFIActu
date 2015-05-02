611a09c5f2df02dc79353c9cd6d2c828eb0c4db0
Laurent Abbal <laurent@abbal.com>
2015-05-02 12:53:10 +0800

    Cosmetique

--- www/js/xmlparser.js
+++ www/js/xmlparser.js
@@ -20,10 +20,8 @@ function xmlparser()
 			{
 				xml_content += '<i id="close_' + i + '" onclick="read('+ i +')" class="grey-text small mdi-navigation-close close_button"></i>';
 				xml_content += '<div class="row">';
-				
-				xml_content += '<div id="date_' + i + '" class="col s2 center" style="padding-top:5px;"><i class="grey-text text-darken-1 small mdi-notification-event-note"></i> <span class="grey-text" style="font-size:70%;">' + items[i].getElementsByTagName("pubDate")[0].textContent.substring(0,10) + '</span></div>';
+				xml_content += '<div id="date_' + i + '" class="col s2 center" style="padding-top:5px;"><i class="grey-text text-darken-1 small mdi-notification-event-note"></i><div class="grey-text" style="margin-top:-5px;font-size:70%;">' + items[i].getElementsByTagName("pubDate")[0].textContent.substring(5,10) + '</div></div>';
 				xml_content += '<div id="title_' + i + '" class="col s10 item_title" onclick="read('+ i +')"><h5>' + items[i].getElementsByTagName("title")[0].textContent + '</h5></div>';
-				
 				xml_content += '<div id="' + i + '" class="col s12 item_description">' + items[i].getElementsByTagName("description")[0].textContent + '</div>';
 				xml_content += '</div>';
 			}
