d0128799468ffca79ca0795dead3a441c64d344b
Laurent Abbal <laurent@abbal.com>
2015-05-02 10:57:57 +0800

    Premiere maquette

--- www/js/xmlparser.js
+++ www/js/xmlparser.js
@@ -18,8 +18,9 @@ function xmlparser()
 									
 			for (i=0;i<items.length;i++)
 			{
+				xml_content += '<i id="close_' + i + '" onclick="read('+ i +')" class="grey-text small mdi-navigation-close close_button"></i>';
 				xml_content += '<div id="title_' + i + '" class="item_title" onclick="read('+ i +')"><h5>' + items[i].getElementsByTagName("title")[0].textContent + '</h5></div>';
-				xml_content += '<div id="date_' + i + '"style="text-align:center;><i class="fa fa-calendar-o"></i>' + items[i].getElementsByTagName("pubDate")[0].textContent.substring(0,10) + '</div>';
+				xml_content += '<div id="date_' + i + '" style="text-align:center;"><i class="mdi-notification-event-note"></i>' + items[i].getElementsByTagName("pubDate")[0].textContent.substring(0,10) + '</div>';
 				xml_content += '<div id="' + i + '" class="item_description">' + items[i].getElementsByTagName("description")[0].textContent + '</div>';
 			}
 			
