7a95f4d0725f58128be0a9b0afe97bcf62d3febe
Laurent Abbal <laurent@abbal.com>
2015-05-03 18:42:13 +0800

    Cosmetique

--- www/js/xmlparser.js
+++ www/js/xmlparser.js
@@ -3,8 +3,8 @@ function xmlparser()
 {
 	var xmlhttp, xml_data, items, content, article, grid;
 	xmlhttp=new XMLHttpRequest();
-	/*xmlhttp.open("GET", "feed.xml", true);*/
-	xmlhttp.open("GET", "http://www.lfihk.net/lfi-feed/lfi-feed-fr.xml", true);
+	xmlhttp.open("GET", "feed.xml", true);
+	/*xmlhttp.open("GET", "http://www.lfihk.net/lfi-feed/lfi-feed-fr.xml", true);*/
 	xmlhttp.overrideMimeType('text/xml; charset=iso-8859-1');
 	
 	xmlhttp.onreadystatechange=function()
@@ -18,7 +18,8 @@ function xmlparser()
 									
 			for (i=0;i<items.length;i++)
 			{
-				xml_content += '<a href="#a_' + i + '" id="close_' + i + '" onclick="read('+ i +')" class="close_button"><i class="grey-text small mdi-navigation-close"></i></a>';
+				//xml_content += '<a href="#a_' + i + '" id="close_' + i + '" onclick="read('+ i +')" class="close_button"><i class="grey-text small mdi-navigation-close"></i></a>';
+				xml_content += '<a href="#a_' + i + '" id="close_' + i + '" onclick="read('+ i +')" class="btn-floating waves-effect waves-light blue-grey darken-3 close_button"><i class="tiny mdi-navigation-close"></i></a>';
 				xml_content += '<a name="a_' + i + '" class="anchor"></a>';
 				xml_content += '<div id="date_' + i + '" class="col s2 center" style="clear:both;padding-top:5px;"><i class="grey-text text-darken-1 small mdi-notification-event-note"></i><div class="grey-text" style="margin-top:-5px;font-size:70%;">' + items[i].getElementsByTagName("pubDate")[0].textContent.substring(5,10) + '</div></div>';
 				xml_content += '<div id="title_' + i + '" class="col s10 item_title"><h5><a href="#a_' + i + '" onclick="read('+ i +')" style="color:#212121;">' + items[i].getElementsByTagName("title")[0].textContent + '</a></h5><br /></div>';
