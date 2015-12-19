a8e2e164865a52b7c2a9ea098dfb3a75d224f775
laurentabbal <laurent@abbal.com>
2015-12-19 12:17:15 +0800

    Access origin

--- www/js/xmlparser.js
+++ www/js/xmlparser.js
@@ -5,11 +5,13 @@ function xmlparser()
 	xmlhttp=new XMLHttpRequest();
 	/*xmlhttp.open("GET", "feed.xml", true);*/
 	/*xmlhttp.open("GET", "http://www.fis.edu.hk/web/WebsiteFeeds.aspx?f=0&lang=fr-fr", true);*/
-
+	xmlhttp.open("GET", "http://www.lfihk.net/lfi-feed/lfi-feed-fr.xml", true);
+	xmlhttp.overrideMimeType('text/xml; charset=iso-8859-1');
 	
 	xmlhttp.onreadystatechange=function()
 	{
-
+		if (xmlhttp.readyState==4 && xmlhttp.status==200)
+		{
 			xml_data = xmlhttp.responseXML;
 			items = xml_data.getElementsByTagName("item"); 
 						
@@ -26,10 +28,8 @@ function xmlparser()
 			}
 			
 			document.getElementById('xml_content').innerHTML=xml_content;
-
+		}
 	}
-	xmlhttp.open("GET", "http://www.lfihk.net/lfi-feed/lfi-feed-fr.xml", true);
-	xmlhttp.overrideMimeType('text/xml; charset=iso-8859-1');
 	xmlhttp.send();
 }
 window.onload = xmlparser();
