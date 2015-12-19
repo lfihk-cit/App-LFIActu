29de4bca70b6d89c4e49462d0741661e05454c1b
laurentabbal <laurent@abbal.com>
2015-12-19 11:42:42 +0800

    Update

--- www/js/xmlparser.js
+++ www/js/xmlparser.js
@@ -3,7 +3,8 @@ function xmlparser()
 {
 	var xmlhttp, xml_data, items, content, article, grid;
 	xmlhttp=new XMLHttpRequest();
-	xmlhttp.open("GET", "http://www.lfihk.net/lfi-feed/lfi-feed-fr.xml", true);
+	xmlhttp.open("GET", "feed.xml", true);
+	/*xmlhttp.open("GET", "http://www.lfihk.net/lfi-feed/lfi-feed-fr.xml", true);*/
 	xmlhttp.overrideMimeType('text/xml; charset=iso-8859-1');
 	
 	xmlhttp.onreadystatechange=function()
